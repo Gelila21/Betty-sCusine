4fd734d5b878869d09f4933728c75b1400cbc103
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-05-07 21:33:32 -0700

    more updates

---
+++ src/submit.php
@@ -0,0 +1,30 @@
+<?php
+// Connect to the database
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "reservation_system";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Get the form data
+$datetime = $_POST["datetime"];
+$name = $_POST["name"];
+$phone = $_POST["phone"];
+$seats = $_POST["seats"];
+
+// Insert the data into the database
+$sql = "INSERT INTO reservations (datetime, name, phone, seats) VALUES ('$datetime', '$name', '$phone', '$seats')";
+if (mysqli_query($conn, $sql)) {
+  echo "Reservation saved successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
